5550e5b7e95556fbc871f634747d9d9aa98dc9b4
Ken Youens-Clark <kyclark@gmail.com>
2019-02-25 09:44:19 -0700

    hpc examples

--- lectures/09-python-parsing/examples/subset_fastx.py
+++ lectures/09-python-parsing/examples/subset_fastx.py
@@ -14,7 +14,7 @@ from Bio import SeqIO
 def get_args():
     """get args"""
     parser = argparse.ArgumentParser(
-        description='Split FASTA files',
+        description='Subset FASTA files',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
     parser.add_argument('file', help='Input file', metavar='FILE')
@@ -39,20 +39,26 @@ def get_args():
     parser.add_argument(
         '-n',
         '--num',
-        help='Number of records per file',
+        help='Number of sequences to take',
         type=int,
         metavar='NUM',
         default=500000)
 
     parser.add_argument(
         '-o',
-        '--outdir',
-        help='Output directory',
+        '--outfile',
+        help='Output file',
         type=str,
-        metavar='DIR',
+        metavar='FILE',
         default='subset')
 
+    parser.add_argument(
+        '--force',
+        help='Force overwrite of existing file',
+        action='store_true')
+
     return parser.parse_args()
+
 
 # --------------------------------------------------
 def warn(msg):
@@ -74,23 +80,31 @@ def main():
     in_file = args.file
     in_fmt = args.infmt
     out_fmt = args.outfmt if args.outfmt else args.infmt
-    out_dir = args.outdir
+    out_file = args.outfile
     num_seqs = args.num
 
     if not os.path.isfile(in_file):
         die('--file "{}" is not a file'.format(in_file))
 
-    if os.path.dirname(os.path.abspath(in_file)) == os.path.abspath(out_dir):
-        die('--outdir "{}" cannot be the same as input files'.format(out_dir))
+    if out_file == in_file:
+        die('--outfile "{}" cannot be the same as input file'.format(out_file))
 
     if num_seqs < 1:
         die("--num cannot be less than one")
 
-    if not os.path.isdir(out_dir):
-        os.mkdir(out_dir)
+    if os.path.isfile(out_file) and not args.force:
+        while True:
+            answer = input(
+                '--outfile "{}" exists. Overwrite [yes|no]? '.format(
+                    out_file)).lower()
+            if answer == 'no':
+                print('Bye')
+                sys.exit(1)
+            elif answer == 'yes':
+                break
+            else:
+                print('Please answer yes or no')
 
-    basename = os.path.basename(in_file)
-    out_file = os.path.join(out_dir, basename)
     out_fh = open(out_file, 'wt')
     num_written = 0
 
